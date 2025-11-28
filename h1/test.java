f127a43a750dd571e89c881f6287aef9236209ad
Jallartz <7qtsknjrvy@privaterelay.appleid.com>
2025-11-28 14:49:17 +0100

    comitt

---
+++ h1/test.java
@@ -0,0 +1,13 @@
+package h1;
+    public class test {
+        public static void main(String[] args) {
+
+            int i = 5, j = 5, z = 22;
+            int k;
+            k = i + j + z;
+            if (k == 22) { System.out.println("k hat den wert " + k);} 
+            else { System.out.println("k ist ungleich 22"); }
+
+    
+    }
+}
